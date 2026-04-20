45c7435f040d12a1a55f7ab167818791aa3bf32e
Ed <62310404+Phospo@users.noreply.github.com>
2026-04-20 23:54:46 +0200

    Add qt before doing anything about frontend.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
+#include <QCoreApplication>
+
 #include "csimulatorfbd.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+    QCoreApplication app(argc, argv);
+
     CSimulatorFBD simulator;
     simulator.run();
     return 0;
